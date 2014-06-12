b243bb3fedce84c3e2ec62b1e0550569bea706a0
Giovanni Gaurfi <giovanni.garufi@fwr.it>
2014-06-12 23:38:50 +0200

    Core
    Signed-off-by: Giovanni Gaurfi <giovanni.garufi@fwr.it>

--- monte-carlo/src/monte_carlo/core.clj
+++ monte-carlo/src/monte_carlo/core.clj
@@ -1,16 +1,59 @@
 (ns monte-carlo.core
   (:gen-class))
 
+
 (defn gen-rand-seq
-  ([] (repeatedly 100 #(rand-int 2))))
+  ([] (repeatedly 1000 #(rand-int 2))))
+
+(defn check
+  [bankroll  i]
+  (if (pos? i)
+    (do 1)
+    (do (if (neg? bankroll)
+          (do (* 2 bankroll))
+          (do -1)
+        )
+    )
+  )
+)
+
+(defn deposit
+  [bankroll bank]
+    (if (> bankroll 0)
+        (do (inc bank))
+        (do bank)
+    )
+)
+
+(defn simulate
+  [rand-seq]
+  (loop [i 0
+        bankroll -1
+        bank 0]
+    (if (< i (count rand-seq))
+      (do (recur (inc i) (check bankroll (nth rand-seq i)) (deposit bankroll bank)))
+      (do (- bank (- (* bankroll bankroll) 1)))
+    )
+  )
+)
 
 
 
 (defn -main
-  "Accepts an int and returns a list of n sequences of 100 random coin flips"
+  "Accepts an int and returns a list of n sequences of 1000 random coin flips"
   [& args]
   (if (empty? args)
-    (do (println (repeatedly 10 gen-rand-seq)))
-    (do (println (repeatedly (read-string (first  args)) gen-rand-seq)))
+    (do (println (reduce + (map simulate (repeatedly 1000 gen-rand-seq)))))
+    (do (println (reduce + (map simulate (repeatedly (read-string (first  args)) gen-rand-seq)))))
+  )
+)
+
+(defn mytest
+  []
+  (let [bankroll 1]
+    (if true
+      (do (inc bankroll))
+      (do (dec bankroll))
+    )
   )
 )
